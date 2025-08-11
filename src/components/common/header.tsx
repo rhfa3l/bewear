b8a05167e44b862f8f89f8e8c81b1f743dc144f4
rhfa3l <rhfa3l@gmail.com>
2025-08-11 13:19:02 -0300

    feat: add home page link in header logo

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -21,7 +21,9 @@ export const Header = () => {
 
   return (
     <header className="flex items-center justify-between p-5">
-      <Image src="/logo.svg" alt="BEWARE" width={100} height={26.14} />
+      <Link href="/">
+        <Image src="/logo.svg" alt="BEWARE" width={100} height={26.14} />
+      </Link>
       <div className="flex items-center">
         <Sheet>
           <SheetTrigger asChild>
